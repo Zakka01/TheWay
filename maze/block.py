da197da319b3f97c8e1ef4dd95abd0a0a9855bb5
zakariae ahrabare <zahrabar@c1r4p4.1337.ma>
2026-04-03 20:32:06 +0100

    modify the random_loops function to remove N walls

--- maze/block.py
+++ maze/block.py
@@ -1,22 +1,16 @@
 class Block:
 
     def __init__(self, x: int, y: int):
-
         """
-            Initialize the attributes : 
-            x, y => ofc the coordinates, 
-            walls => if the block closed or not
-            checked => if the block already visited or not
-            ...
+        Initialize the attributes :
+        x, y => ofc the coordinates,
+        walls => if the block closed or not
+        checked => if the block already visited or not
+        ...
         """
         self.x = x
         self.y = y
-        self.walls = {
-            "top": True,
-            "bottom": True,
-            "left": True,
-            "right": True
-        }
+        self.walls = {"top": True, "bottom": True, "left": True, "right": True}
         self.checked = False
         self.is_pattern = False
         self.is_path = False
@@ -24,10 +18,9 @@ class Block:
         self.visited_by_bfs = False
 
     def has_wall(self, direction: str) -> bool:
-
-        """ 
-            check if block has wall in the given direction
-            and return True if yes , otherwise False
+        """
+        check if block has wall in the given direction
+        and return True if yes , otherwise False
         """
         if self.walls[direction] is True:
             return True
@@ -35,7 +28,7 @@ class Block:
 
     def pop_wall(self, direction: str) -> None:
         """
-            Mark the Direction as 'False' to Create Edge 
-            and Make the Two Blocks Connected
+        Mark the Direction as 'False' to Create Edge
+        and Make the Two Blocks Connected
         """
         self.walls[direction] = False
